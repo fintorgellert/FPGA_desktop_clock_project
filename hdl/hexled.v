f3e8b24ba5fb2c1efa5644c28a7ee932b14959d5
fintorgellert <99272714+fintorgellert@users.noreply.github.com>
2025-12-01 16:38:46 +0100

    Clean up comments in hexled.v
    Removed header comments from hexled.v

--- hdl/hexled.v
+++ hdl/hexled.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.10.2025 14:04:11
-// Design Name: 
-// Module Name: hexled
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-// File: hexled.v
 module hexled(
     input [5:0] val,
     input rst,
